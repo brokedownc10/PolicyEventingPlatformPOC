0a561ba68fa869b832d747e70f2e467ff3d8e41c
TIMOTHY PIROZZI <Timothy.Pirozzi@LibertyMutual.com>
2021-07-30 09:02:42 -0400

    update

--- service/src/lmpolicyformsource.py
+++ service/src/lmpolicyformsource.py
@@ -51,4 +51,4 @@ if __name__ == "__main__":
     parser.add_argument("url", help="'url'")
     args = parser.parse_args()
 
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
